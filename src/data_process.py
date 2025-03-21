1ef67d1371b6e761f9a09e3d5a0d2045dcba514d
v.fomenko <vladislav.fomenko.2021@yandex.ru>
2025-03-21 15:00:24 +0300

    fix paths for HEEDS

--- src/data_process.py
+++ src/data_process.py
@@ -2,11 +2,21 @@ from pathlib import Path
 import xmltodict
 # import json
 import math
+import importlib.util
+import sys
+
+module_path = "D:\\Files\\Work\\Hydrodynamic\\leaks\\src\\units.py"
+
+module_name = "units"
+spec = importlib.util.spec_from_file_location(module_name, module_path)
+module = importlib.util.module_from_spec(spec)
+sys.modules[module_name] = module
+spec.loader.exec_module(module)
+
 from units import *
 
-
 class Pump:
-    __data_path = Path(__file__).resolve().parent.parent / "data"
+    __data_path = Path(__file__).resolve().parent.parent / "data" / "xml"
     __data = {}
     __set_up = {}
     __leaks = {}
--- src/leaks_calc.py
+++ src/leaks_calc.py
@@ -1,3 +1,15 @@
+import sys
+import importlib.util
+import sys
+
+module_path = "D:\\Files\\Work\\Hydrodynamic\\leaks\\src\\data_process.py"
+
+module_name = "data_process"
+spec = importlib.util.spec_from_file_location(module_name, module_path)
+module = importlib.util.module_from_spec(spec)
+sys.modules[module_name] = module
+spec.loader.exec_module(module)
+
 from data_process import Pump
 
 Pump.load_params()
